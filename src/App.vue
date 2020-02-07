d4c1105f43108b403b392c1cbfc4f117b2b2c63a
markkizz <mark.pattanapara@gmail.com>
2020-02-07 22:52:17 +0700

    Add dependencies and setup style

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="header"></div>
+    <Todos />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todo';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Todos
+  },
+  data() {
+    return {
+      todo: [
+        {
+          id: 1,
+          title: "First Todo",
+          isComplete: false
+        },
+        {
+          id: 1,
+          title: "First Todo",
+          isComplete: false
+        }
+      ]
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .container{
+    padding:10px;
+    width:380px;
+    margin:0 auto;
+  }
+  .header{
+    width: 380px;
+    height:200px;
+    background-image: url('./assets/images.jpg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    border-radius: 15px 15px 0 0;
+    position: relative;
+}
+.clear{
+    width : 30px;
+    height: 30px;
+    position: absolute;
+    right:20px;
+    top: 20px;
+}
+.clear i{
+    font-size: 30px;
+    color: #FFF;
+}
+.clear i:hover{
+    cursor: pointer;
+    text-shadow: 1px 3px 5px #000;
+    transform: rotate(45deg);
 }
 </style>
